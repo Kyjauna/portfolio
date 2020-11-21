0496efd9f7c449a0638fc43b8ab42f1095117bbd
Kyjauna <kyjauna.marshall@mail.mcgill.ca>
2020-11-21 11:05:04 -0500

    Updates

---
+++ precache-manifest.f9741799a082376870a6ef088b1dae11.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91c6f5d40e31d4a72e935000906165e",
+    "url": "/personal_website/index.html"
+  },
+  {
+    "revision": "e6aa44d94d7a5812bc70",
+    "url": "/personal_website/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "069af14e9ded8c9e885e",
+    "url": "/personal_website/static/css/main.1faef398.chunk.css"
+  },
+  {
+    "revision": "e6aa44d94d7a5812bc70",
+    "url": "/personal_website/static/js/2.c341b0fc.chunk.js"
+  },
+  {
+    "revision": "a7aec927f1e863a53de22e58e5329447",
+    "url": "/personal_website/static/js/2.c341b0fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "069af14e9ded8c9e885e",
+    "url": "/personal_website/static/js/main.14e821e3.chunk.js"
+  },
+  {
+    "revision": "0bc607e18bf4bd1cb917",
+    "url": "/personal_website/static/js/runtime-main.d42ff198.js"
+  },
+  {
+    "revision": "8a728b638c190ff3d43d689e0f6d852d",
+    "url": "/personal_website/static/media/aboutme1.8a728b63.PNG"
+  },
+  {
+    "revision": "31db5b798786f21b2f9f559c084880c3",
+    "url": "/personal_website/static/media/aboutme2.31db5b79.PNG"
+  },
+  {
+    "revision": "56204edd1c5421069086d9b8b6e40099",
+    "url": "/personal_website/static/media/background.56204edd.PNG"
+  },
+  {
+    "revision": "f0fb63969440d862b9cc2018bc469679",
+    "url": "/personal_website/static/media/cv1.f0fb6396.PNG"
+  },
+  {
+    "revision": "cd0c72f19c396ca0e224303f845fc8e3",
+    "url": "/personal_website/static/media/cv2.cd0c72f1.PNG"
+  },
+  {
+    "revision": "a8203db311b81e239f508ccfa35dc34b",
+    "url": "/personal_website/static/media/project1.a8203db3.PNG"
+  },
+  {
+    "revision": "7e8e485c24b16200956e9b879131e686",
+    "url": "/personal_website/static/media/project2.7e8e485c.PNG"
+  }
+]);
